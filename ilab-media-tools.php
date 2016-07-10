a8046de05cdbf750e3bfae47040bea50d0afb2d3
Jon Gilkison <jon.gilkison@gmail.com>
2016-07-10 08:15:13 +0700

    Deactivate the plugin if amazon-web-services plugin is activated.

--- ilab-media-tools.php
+++ ilab-media-tools.php
@@ -8,6 +8,22 @@ Version: 0.1
 Author URI: http://interfacelab.com
 */
 
+
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+if (is_plugin_active('amazon-web-services/amazon-web-services.php')) {
+	deactivate_plugins( plugin_basename( __FILE__ ) );
+
+	add_action( 'admin_notices', function () {
+		?>
+		<div class="notice notice-error is-dismissible">
+			<p><?php _e( 'ILAB Media Tools cannot be activated the same time as the <strong>Amazon Web Services</strong> plugin due to ILAB Media Tools using a newer version of the Amazon AWS SDK.', 'ilab-media-tools' ); ?></p>
+		</div>
+		<?php
+	} );
+	return;
+}
+
 // Directory defines
 define('ILAB_TOOLS_DIR',dirname(__FILE__));
 define('ILAB_HELPERS_DIR',ILAB_TOOLS_DIR.'/helpers');
@@ -20,6 +36,9 @@ $plug_url = plugin_dir_url( __FILE__ );
 define('ILAB_PUB_JS_URL',$plug_url.'public/js');
 define('ILAB_PUB_CSS_URL',$plug_url.'public/css');
 define('ILAB_PUB_IMG_URL',$plug_url.'public/img');
+
+// Composer
+require_once(ILAB_VENDOR_DIR.'/autoload.php');
 
 // Helper functions
 require_once('helpers/ilab-media-tool-helpers.php');
