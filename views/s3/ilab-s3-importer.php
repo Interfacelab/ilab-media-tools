fce523edfcb32b5bcb8775e4c7ea58b0404d6ef6
Jon Gilkison <jon.gilkison@gmail.com>
2017-09-09 03:09:04 +0700

    Remove shell command to determine mime type.

--- views/s3/ilab-s3-importer.php
+++ views/s3/ilab-s3-importer.php
@@ -17,6 +17,11 @@
 		height: 100%;
 		width: {{$progress}}%;
 	}
+
+    .tool-disabled {
+        padding: 10px 15px;
+        border: 1px solid #df8403;
+    }
 </style>
 <div class="wrap">
 	<h1>S3 Importer</h1>
@@ -30,7 +35,11 @@
 			<li>You <strong>MUST HAVE</strong> S3 enabled and working in <a href="admin.php?page=media-tools-top">Tools Settings</a> before running this task.</li>
 		</ol>
 		<div style="margin-top: 2em;">
+            <?php if($enabled): ?>
 			<a href="#" class="ilab-ajax button">Import Uploads</a>
+            <?php else: ?>
+                <strong class="tool-disabled">Please <a href="admin.php?page=media-tools-top">enable S3</a> before using this tool.</strong>
+            <?php endif ?>
 		</div>
 	</div>
 	<div id="s3-importer-progress" {{($status!="running") ? 'style="display:none"':''}}>
