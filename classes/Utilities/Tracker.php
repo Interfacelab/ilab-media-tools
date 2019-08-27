9a77e33a456e91fbd781ab546b7a88cacea25713
Jon Gilkison <jon.gilkison@gmail.com>
2019-08-27 13:20:41 +0700

    Switched feature tracker to Amplitude

--- classes/Utilities/Tracker.php
+++ classes/Utilities/Tracker.php
@@ -14,44 +14,29 @@
 namespace ILAB\MediaCloud\Utilities;
 
 use ILAB\MediaCloud\Tools\ToolsManager;
-use TheIconic\Tracking\GoogleAnalytics\Analytics;
+use Zumba\Amplitude\Amplitude;
 
 if (!defined( 'ABSPATH')) { header( 'Location: /'); die; }
 
 final class Tracker {
-	private const CLOUD_STORAGE_ENABLED = 1;
-	private const IMGIX_ENABLED = 2;
-	private const DYNAMIC_IMAGES_ENABLED = 3;
-	private const DIRECT_UPLOADS_ENABLED = 4;
-	private const VISION_ENABLED = 5;
-	private const ASSETS_ENABLED = 6;
-	private const CROP_ENABLED = 7;
-	private const STORAGE_PROVIDER = 8;
-	private const VISION_PROVIDER = 9;
-	private const ASSETS_MODE = 10;
-
-	private const TOOLS_MAP = [
-		'storage' => self::CLOUD_STORAGE_ENABLED,
-		'imgix' => self::IMGIX_ENABLED,
-		'glide' => self::DYNAMIC_IMAGES_ENABLED,
-		'media-upload' => self::DIRECT_UPLOADS_ENABLED,
-		'vision' => self::VISION_ENABLED,
-		'assets' => self::ASSETS_ENABLED,
-		'crop' => self::CROP_ENABLED,
+	const TRACKED_TOOLS = [
+		'storage',
+		'imgix',
+		'glide',
+		'media-upload',
+		'vision',
+		'assets',
+		'crop'
 	];
-
 
 	/**
 	 * Creates the Analytics logger class
 	 *
-	 * @return Analytics
+	 * @return Amplitude
 	 */
 	private static function createAnalytics() {
-		$analytics = new Analytics(true);
-		$analytics
-			->setProtocolVersion(1)
-			->setTrackingId('UA-140883146-5')
-			->setDocumentHostName('plugin.mediacloud.press');
+		/** @var \Freemius $media_cloud_licensing */
+		global $media_cloud_licensing;
 
 		$clid = Environment::Option('mcloud-tracking-id', null, null);
 		if (empty($clid)) {
@@ -59,40 +44,32 @@ final class Tracker {
 			Environment::UpdateOption('mcloud-tracking-id', $clid);
 		}
 
-		$analytics->setUserId($clid);
+		$amplitude =Amplitude::getInstance();
+		$amplitude->init('ad858aa2384b66a11734161e4331287a', $clid);
 
-		return $analytics;
-	}
-
-	public static function trackSettings() {
-		if (!LicensingManager::OptedIn('mcloud-opt-usage-tracking')) {
-			return;
-		}
-
-		$analytics = self::createAnalytics();
+		$userProps = [
+			'plan' => $media_cloud_licensing->get_plan_name()
+		];
 
 		$assetsEnabled = false;
 		foreach(ToolsManager::instance()->tools as $toolId => $tool) {
-			if (isset(self::TOOLS_MAP[$toolId])) {
-				$index = self::TOOLS_MAP[$toolId];
+			if (in_array($toolId, self::TRACKED_TOOLS)) {
 				$enabled = $tool->enabled();
-
+				$userProps[$toolId] = $enabled;
 				if ($toolId == 'assets') {
 					$assetsEnabled = $enabled;
 				}
-
-				$analytics->setCustomDimension($enabled, $index);
 			}
 		}
 
 		$provider = Environment::Option('mcloud-storage-provider','ILAB_CLOUD_STORAGE_PROVIDER', null);
 		if (!empty($provider)) {
-			$analytics->setCustomDimension($provider, self::STORAGE_PROVIDER);
+			$userProps['storage-provider'] = $provider;
 		}
 
 		$visionProvider = Environment::Option('mcloud-vision-provider','ILAB_VISION_PROVIDER', null);
 		if (!empty($visionProvider)) {
-			$analytics->setCustomDimension($visionProvider, self::VISION_PROVIDER);
+			$userProps['vision-provider'] = $provider;
 		}
 
 		if ($assetsEnabled) {
@@ -100,19 +77,23 @@ final class Tracker {
 			$storeJS = Environment::Option('mcloud-assets-store-js', null, false);
 
 			if (!empty($storeCSS) && !empty($storeJS)) {
-				$mode = 'Push';
+				$mode = 'push';
 			} else if (empty($storeCSS) && empty($storeJS)) {
-				$mode = 'Pull';
+				$mode = 'pull';
 			} else {
-				$mode = 'Mixed';
+				$mode = 'mixed';
 			}
 
-			$analytics->setCustomDimension($mode, self::ASSETS_MODE);
+			$userProps['asset-mode'] = $mode;
 		}
 
-		$analytics->setDocumentPath('/settings/update');
-		$analytics->setDocumentTitle('Update Settings');
-		$analytics->sendPageview();
+		$amplitude->setUserProperties($userProps);
+
+		return $amplitude;
+	}
+
+	public static function trackSettings() {
+		self::trackView('Update Settings', '/settings/update');
 	}
 
 	public static function trackView($title, $page) {
@@ -120,11 +101,12 @@ final class Tracker {
 			return;
 		}
 
-		$analytics = self::createAnalytics();
-
-		$analytics->setDocumentPath($page);
-		$analytics->setDocumentTitle($title);
-		$analytics->sendPageview();
+		$amplitude = self::createAnalytics();
+		$amplitude->queueEvent($page, [
+			'app_version' => MEDIA_CLOUD_VERSION,
+			'ip' => '$remote'
+		]);
+		$amplitude->logQueuedEvents();
 	}
 
 }
