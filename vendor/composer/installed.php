f74309571a7e4068bae6e5b638dae8a2d2fddaa1
Jon Gilkison <jon.gilkison@gmail.com>
2022-10-07 13:30:00 +0700

    re-added the vendor directory

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'ilab/ilab-media-tools',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ilab/ilab-media-tools' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
